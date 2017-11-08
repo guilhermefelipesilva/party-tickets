44f693b98bcac39786a44ab83a22eb8f7c102c15
guilhermefelipesilva <guilhermefelipesilva@gmail.com>
2017-11-08 00:07:20 -0200

    Començando a implementação de rotas e navegação

--- App.js
+++ App.js
@@ -1,23 +1,103 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, View, Image } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import Expo from 'expo'
+import { Container, Header, Content, Card, CardItem, Thumbnail, Button, Text, Icon, Left, Body, Right } from 'native-base';
 
-export default class App extends React.Component {
+class PartyCard extends React.Component {
+
+  constructor(props){
+    super(props)
+  }
+
+  render() {
+    const navigation = this.props.navigation
+    console.info(navigation)
+    return (
+      <Card>
+        <CardItem>
+          <Left>
+            <Thumbnail source={{ uri: 'http://www.fundosanimais.com/Imagens/imagens-lobos.jpg' }} />
+            <Body>
+              <Text>House Winter</Text>
+              <Text note>Festa</Text>
+            </Body>
+          </Left>
+        </CardItem>
+        <CardItem cardBody>
+          <Image source={{ uri: 'https://sis.quero2ingressos.com.br/imagem/logo/L00192.jpg' }} style={{ height: 200, width: null, flex: 1 }} />
+        </CardItem>
+        <CardItem>
+          <Left>
+            <Text transparent>12 vendidos</Text>
+          </Left>
+          <Right>
+            <Button onPress={() => navigation.navigate('PartyDetail')}>
+              <Text>Comprar</Text>
+            </Button>
+          </Right>
+        </CardItem>
+      </Card>
+    )
+  }
+}
+
+class PartiesScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Festas'
+  };
+
+  render() {
+    // const { navigate } = this.props.navigation;
+    return (
+      <Container>
+        <Content>
+          <PartyCard navigation={this.props.navigation}/>
+        </Content>
+      </Container>
+    );
+  }
+}
+
+class PartyDetailScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Detalhes da Festa',
+  };
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View>
+        <Text>Detalhes da Festa!</Text>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const PartyTicketsApp = StackNavigator({
+  Parties: { screen: PartiesScreen },
+  PartyDetail: { screen: PartyDetailScreen },
 });
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isReady: false
+    };
+  }
+
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      'Roboto': require('native-base/Fonts/Roboto.ttf'),
+      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+    });
+    this.setState({ isReady: true });
+  }
+
+  render() {
+    if (!this.state.isReady) {
+      return <Expo.AppLoading />;
+    }
+
+    return <PartyTicketsApp />;
+  }
+}
