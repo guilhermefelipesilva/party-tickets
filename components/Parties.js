1c1f4b528db3a7dc02c2685174bd89ecae47d97c
guilhermefelipesilva <guilhermefelipesilva@gmail.com>
2017-11-13 23:11:10 -0200

    implements axios para requests e props em component

--- components/Parties.js
+++ components/Parties.js
@@ -1,20 +1,45 @@
 import React, { Component } from 'react';
 import { Container, Content } from 'native-base';
-import { PartyCard } from './PartyCard'
+import { PartyCard } from './PartyCard';
+import axios from 'axios'
 
 export class PartiesScreen extends React.Component {
-    static navigationOptions = {
-      title: 'Festas'
-    };
-  
-    render() {
-      return (
-        <Container>
-          <Content>
-            <PartyCard navigation={this.props.navigation}/>
-          </Content>
-        </Container>
-      );
-    }
+
+  constructor(props) {
+    super(props);
+    this.state = { parties: [] };
   }
-  
+
+  componentWillMount() {
+    this.getParties()
+    .then(data => {
+      this.state.parties = data;
+    })
+  }
+
+  static navigationOptions = {
+    title: 'Festas'
+  };
+
+  render() {
+    console.log(this.state)
+    return (
+      <Container>
+        <Content>
+          {
+            this.state.parties.map((party, key) => (
+              <PartyCard key={party.id} navigation={this.props.navigation} party={party} />
+            ))
+          }
+        </Content>
+      </Container>
+    );
+  }
+
+  getParties() {
+    return axios.get("http://www.mocky.io/v2/5a0a3fa32e00000c14489c47")
+      .then(response => {
+        return response.data
+      })
+  }
+}
--- components/PartyCard.js
+++ components/PartyCard.js
@@ -3,38 +3,38 @@ import { Image } from 'react-native';
 import { Container, Header, Content, Card, CardItem, Thumbnail, Button, Text, Icon, Left, Body, Right } from 'native-base';
 
 export class PartyCard extends React.Component {
-    
-      constructor(props){
-        super(props)
-      }
-    
-      render() {
-        const navigation = this.props.navigation
-        return (
-          <Card>
-            <CardItem>
-              <Left>
-                <Thumbnail source={{ uri: 'http://www.fundosanimais.com/Imagens/imagens-lobos.jpg' }} />
-                <Body>
-                  <Text>House Winter</Text>
-                  <Text note>Festa</Text>
-                </Body>
-              </Left>
-            </CardItem>
-            <CardItem cardBody>
-              <Image source={{ uri: 'https://sis.quero2ingressos.com.br/imagem/logo/L00192.jpg' }} style={{ height: 200, width: null, flex: 1 }} />
-            </CardItem>
-            <CardItem>
-              <Left>
-                <Text transparent>12 vendidos</Text>
-              </Left>
-              <Right>
-                <Button onPress={() => navigation.navigate('PartyDetail')}>
-                  <Text>Comprar</Text>
-                </Button>
-              </Right>
-            </CardItem>
-          </Card>
-        )
-      }
-    }
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const navigation = this.props.navigation
+    return (
+      <Card>
+        <CardItem>
+          <Left>
+            <Thumbnail source={{ uri: this.props.party.tumbImg }} />
+            <Body>
+              <Text>{this.props.party.name}</Text>
+              <Text note>{this.props.party.type}</Text>
+            </Body>
+          </Left>
+        </CardItem>
+        <CardItem cardBody>
+          <Image source={{ uri: this.props.party.partyImg }} style={{ height: 200, width: null, flex: 1 }} />
+        </CardItem>
+        <CardItem>
+          <Left>
+            <Text transparent> {this.props.party.ticketsSold} vendidos</Text>
+          </Left>
+          <Right>
+            <Button onPress={() => navigation.navigate('PartyDetail', { idParty: this.props.party.id })}>
+              <Text>Comprar</Text>
+            </Button>
+          </Right>
+        </CardItem>
+      </Card>
+    )
+  }
+}
