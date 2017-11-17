9c6a0dbb6ad67e5f9f1247a2b28022a8ec5bcb05
guilhermefelipesilva <guilhermefelipesilva@gmail.com>
2017-11-16 23:23:35 -0200

    adicionado carousel

--- components/PartyDetail.js
+++ components/PartyDetail.js
@@ -1,11 +1,14 @@
 
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
+import { Container, Header, Content } from 'native-base';
 import axios from 'axios'
+import Carousel from 'simple-carousel-react-native'
 
 export class PartyDetailScreen extends React.Component {
 
     constructor(props) {
+        console.log(props)
         super(props)
     }
 
@@ -33,9 +36,35 @@ export class PartyDetailScreen extends React.Component {
 
     render() {
         return (
-            <View>
-                <Text>Detalhes da Festa!</Text>
-            </View>
+            <Container>
+                <Carousel
+                    showScroll={false}
+                    showBubbles={false}
+                    width={400}
+                    bubbleHeight={0}
+                    bubbleWidth={0}
+                    backgroundColor={'red'}
+                    height={200}>
+                    <View style={{ flex: 1 }}>
+                        <Image
+                            resizeMode="stretch"
+                            source={{ uri: "http://www.fundosanimais.com/Imagens/imagens-lobos.jpg" }}
+                            style={{
+                                flex: 1,
+                                alignSelf: "stretch"
+                            }}
+                        />
+                    </View>
+
+                    <View>
+                        <Text>
+                            Page 2
+                        </Text>
+                    </View>
+                </Carousel>
+
+            </Container>
+
         );
     }
 }
